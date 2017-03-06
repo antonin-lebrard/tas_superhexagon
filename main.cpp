ac74f6feb56dc564b4fc67051340200ddb5ab0bf
Antonin Lebrard <a.lebrard@gmail.com>
2017-03-06 11:50:21 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,105 +2,72 @@
 #include <cv.h>
 #include "opencv2/highgui/highgui.hpp"
 #include "video.h"
+#include "Raycast.h"
 
 using namespace cv;
 using namespace std;
 
+struct TriangleDetected {
+    vector<Point> points;
+    double perimeterSquared = 0.0;
+};
+
 Mat toDisplayVideo, gray, drawingContour;
 
-int thresh = 100;
-
-Point2i pointsToFetch[45] = {
-
-        Point2i(50,50),
-        Point2i(100,50),
-        Point2i(150,50),
-        Point2i(200,50),
-        Point2i(250,50),
-
-        Point2i(50,100),
-        Point2i(100,100),
-        Point2i(150,100),
-        Point2i(200,100),
-        Point2i(250,100),
-
-        Point2i(50,150),
-        Point2i(100,150),
-        Point2i(150,150),
-        Point2i(200,150),
-        Point2i(250,150),
-
-        Point2i(50,200),
-        Point2i(100,200),
-        Point2i(150,200),
-        Point2i(200,200),
-        Point2i(250,200),
-
-        Point2i(50,250),
-        Point2i(100,250),
-        Point2i(150,250),
-        Point2i(200,250),
-        Point2i(250,250),
-
-        Point2i(50,300),
-        Point2i(100,300),
-        Point2i(150,300),
-        Point2i(200,300),
-        Point2i(250,300),
-
-        Point2i(50,350),
-        Point2i(100,350),
-        Point2i(150,350),
-        Point2i(200,350),
-        Point2i(250,350),
-
-        Point2i(50,400),
-        Point2i(100,400),
-        Point2i(150,400),
-        Point2i(200,400),
-        Point2i(250,400),
-
-};
+bool stopAtThisFrame = false;
+int thresh = 100, countIgnore = 60;
 
 void draw() {
     imshow("scene", toDisplayVideo);
     imshow("Hull demo", drawingContour);
+    //imshow("gray", gray);
     /// callback mouse if necessary
 }
 
-void trianglesDetect(Mat& img, const vector< vector<Point> >& contours){
+void trianglesDetect(Mat& img, const vector<vector<Point>>& contours, vector<TriangleDetected>& trianglesDetected){
     vector<Point> approxTriangle;
     for(size_t i = 0; i < contours.size(); i++){
-        approxPolyDP(contours[i], approxTriangle, arcLength(Mat(contours[i]), true)*0.05, true);
+        approxPolyDP(contours[i], approxTriangle, arcLength(Mat(contours[i]), true)*0.1, true);
         if(approxTriangle.size() == 3){
-            //drawContours(img, contours, i, Scalar(0, 255, 255), CV_FILLED); // fill GREEN
-            vector<Point>::iterator vertex;
-            for(vertex = approxTriangle.begin(); vertex != approxTriangle.end(); ++vertex){
-                circle(img, *vertex, 3, Scalar(0, 0, 255), 1);
+            TriangleDetected triangleDetected;
+            triangleDetected.points = approxTriangle;
+            for (int j = 0; j < 3; j++){
+                if (j > 0)
+                    triangleDetected.perimeterSquared += abs(approxTriangle[j].x - approxTriangle[j-1].x) 
+                                                         + abs(approxTriangle[j].y - approxTriangle[j-1].y);
+            }
+            triangleDetected.perimeterSquared += abs(approxTriangle[2].x - approxTriangle[0].x)
+                                                 + abs(approxTriangle[2].y - approxTriangle[0].y);
+            //cout << "x: " << approxTriangle[0].x << " y: " << approxTriangle[0].y << endl;
+            if (approxTriangle[0].x > 290 && approxTriangle[0].x < 460 && approxTriangle[0].y > 150 && approxTriangle[0].y < 330) {
+                if (triangleDetected.perimeterSquared < 50.0) {
+                    for (int j = 0; j < 3; j++) {
+                        circle(img, approxTriangle[j], 3, Scalar(0, 0, 255), 1);
+                    }
+                    trianglesDetected.push_back(triangleDetected);
+                }
             }
         }
     }
+    if (trianglesDetected.size() == 0)
+        stopAtThisFrame = true;
 }
 
-void thresh_callback(int, void* )
+void thresh_callback()
 {
     Mat threshold_output;
     vector<vector<Point> > contours;
     vector<Vec4i> hierarchy;
 
-    /// Detect edges using Threshold
     threshold( gray, threshold_output, thresh, 255, THRESH_BINARY );
 
-    /// Find contours
     findContours( threshold_output, contours, hierarchy, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE, Point(0, 0) );
 
-    /// Find the convex hull object for each contour
     vector<vector<Point>> hull( contours.size() );
     for( int i = 0; i < contours.size(); i++ ) {
         convexHull( Mat(contours[i]), hull[i], false );
     }
 
-    /// Draw contours + hull results
     drawingContour = Mat::zeros(threshold_output.size(), CV_8UC3 );
     for( int i = 0; i< contours.size(); i++ )
     {
@@ -108,7 +75,9 @@ void thresh_callback(int, void* )
         drawContours( drawingContour, contours, i, color, 1, 8, vector<Vec4i>(), 0, Point() );
         //drawContours( drawingContour, hull, i, color, 1, 8, vector<Vec4i>(), 0, Point() );
     }
-    trianglesDetect(toDisplayVideo, contours);
+    vector<TriangleDetected> trianglesDetected;
+    trianglesDetect(toDisplayVideo, contours, trianglesDetected);
+    
 }
 
 void filterOutNotWall() {
@@ -122,7 +91,6 @@ void filterOutNotWall() {
         }
     }
     /// concentrate into the middle of the image
-    /// the zone is about 50 < x  
     for (int i = 0; i < gray.rows; i++){
         for (int j = 0; j < gray.cols; j++){
             if (i < 50 || j < 150 || j > gray.cols - 210)
@@ -134,18 +102,35 @@ void filterOutNotWall() {
 void doThings(){
     cvtColor(toDisplayVideo, gray, COLOR_RGB2GRAY);
     filterOutNotWall();
-    thresh_callback( 0, 0 );
+    thresh_callback();
+    Raycast::detectCollision(gray, drawingContour, Point2d(-1.0, 0.0), false);
+    Raycast::detectCollision(gray, drawingContour, Point2d(0.0, -1.0), false);
+    Raycast::detectCollision(gray, drawingContour, Point2d(1.0, 0.0), false);
+    Raycast::detectCollision(gray, drawingContour, Point2d(0.0, 1.0), false);
 }
 
 void initThings(){
     cvtColor(toDisplayVideo, gray, COLOR_RGB2GRAY);
     namedWindow("scene", CV_WINDOW_AUTOSIZE);
     namedWindow("Hull demo", CV_WINDOW_AUTOSIZE);
-    createTrackbar(" Threshold:", "Source", &thresh, 255, thresh_callback);
-    thresh_callback( 0, 0 );
+    thresh_callback();
+}
+
+bool stopAtFrame(){
+    bool toReturn = stopAtThisFrame;
+    if (countIgnore != 0){
+        countIgnore--;
+        stopAtThisFrame = false;
+        return false;
+    }
+    if (stopAtThisFrame)
+        stopAtThisFrame = false;
+    return toReturn;
 }
 
 int main() {
-    Video v = Video(initThings, doThings, draw);
+    Video v = Video(initThings, doThings, draw, stopAtFrame);
     v.openvideo("../good30fps.flv", toDisplayVideo);
 }
+
+
