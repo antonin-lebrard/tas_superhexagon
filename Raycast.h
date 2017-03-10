d84f35a4819875653d10d7dc0d0abec52e809c41
Antonin Lebrard <a.lebrard@gmail.com>
2017-03-10 17:38:40 +0100

    Update Raycast.h

--- Raycast.h
+++ Raycast.h
@@ -8,20 +8,22 @@
 #include <iostream>
 #include <cv.h>
 #include "opencv2/highgui/highgui.hpp"
+#include "globals.h"
 
 using namespace std;
 using namespace cv;
 
 class Raycast {
 private:
-    static bool hasColorChanged(Mat& img, uchar& lastColor, Point2i& toCheck);
-    static void progressPointOnLineFromXFormula(Point2i& pointToMove, Point2d& lineVec, Point2i& initialPoint, int& x);
-    static void progressPointOnLineFromYFormula(Point2i& pointToMove, Point2d& lineVec, Point2i& initialPoint, int& y);
-    static bool algoXStopOnCollision(Mat& img, uchar& lastColor, int& nbChangeToIgnore, Point2i& pointToMove, Point2d& lineVec, Point2i& initialPoint, int& x);
-    static bool algoYStopOnCollision(Mat& img, uchar& lastColor, int& nbChangeToIgnore, Point2i& pointToMove, Point2d& lineVec, Point2i& initialPoint, int& y);
+    static bool isPointInsideMat(const Mat& img, const Point2i& toCheck);
+    static bool hasColorChanged(const Mat& img, uchar& lastColor, const Point2i& toCheck);
+    static void progressPointOnLineFromXFormula(Point2i& pointToMove, const Point2d& lineVec, const Point2i& initialPoint, const int& x);
+    static void progressPointOnLineFromYFormula(Point2i& pointToMove, const Point2d& lineVec, const Point2i& initialPoint, const int& y);
+    static bool algoXStopOnCollision(const Mat& img, uchar& lastColor, int& nbChangeToIgnore, Point2i& pointToMove, const Point2d& lineVec, const Point2i& initialPoint, const int& x);
+    static bool algoYStopOnCollision(const Mat& img, uchar& lastColor, int& nbChangeToIgnore, Point2i& pointToMove, const Point2d& lineVec, const Point2i& initialPoint, const int& y);
     
 public:
-    static Point2i detectCollision(Mat& img, Mat& drawing, Point2d lineVec, bool isTriangleOnTheWay);
+    static RaycastHit detectCollision(Mat& img, Mat& drawing, const Point2d lineVec, bool triangleOnTheWay);
 };
 
 
