6e3a0c61a20c4c77d0f5b1a50a7bd26595129c6f
Antonin Lebrard <a.lebrard@gmail.com>
2017-03-10 17:38:24 +0100

    Update Raycast.cpp

--- Raycast.cpp
+++ Raycast.cpp
@@ -7,26 +7,31 @@
 /**
  * Stop at third point not equal to 0
  * @param img grayscale image
- * @param vectorLine 
- * @return the third point
+ * @param drawing where to draw circle in
+ * @param vectorLine direction of the ray
+ * @param isTriangleOnTheWay if triangle is in direction of ray, so should be ignored
+ * @return point of collision with first wall in direction of vectorLine
  */
-Point2i Raycast::detectCollision(Mat& img, Mat& drawing, Point2d vectorLine, bool isTriangleOnTheWay) {
-    Point2i initialPoint = Point2i(img.cols / 2, img.rows / 2);
+RaycastHit Raycast::detectCollision(Mat& img, Mat& drawing, const Point2d vectorLine, bool triangleOnTheWay) {
+    RaycastHit hit = RaycastHit();
+    const Point2i initialPoint = Point2i(img.cols / 2, img.rows / 2);
     Point2i current = Point2i(img.cols / 2, img.rows / 2);
-    double absX = abs(vectorLine.x), absY = abs(vectorLine.y);
+    const double absX = abs(vectorLine.x), absY = abs(vectorLine.y);
     uchar lastColor = img.at<uchar>(initialPoint);
-    int countChangeOfColorToIgnore = 2 + (isTriangleOnTheWay ? 1 : 0);
+    int countChangeOfColorToIgnore = 2 + (triangleOnTheWay ? 2 : 0);
     if (absX > absY){
         if (vectorLine.x < 0.0) {
             for (int i = initialPoint.x; i >= 0; i--) {
                 if (algoXStopOnCollision(img, lastColor, countChangeOfColorToIgnore, current, vectorLine, initialPoint, i))
                     break;
+                circle(drawing, current, 1, Scalar(0, 0, 255), -1);
             }
         }
         else {
             for (int i = current.x; i < img.cols; i++) {
                 if (algoXStopOnCollision(img, lastColor, countChangeOfColorToIgnore, current, vectorLine, initialPoint, i))
                     break;
+                circle(drawing, current, 1, Scalar(0, 0, 255), -1);
             }
         }
     }
@@ -35,20 +40,35 @@ Point2i Raycast::detectCollision(Mat& img, Mat& drawing, Point2d vectorLine, boo
             for (int j = initialPoint.y; j >= 0; j--){
                 if (algoYStopOnCollision(img, lastColor, countChangeOfColorToIgnore, current, vectorLine, initialPoint, j))
                     break;
+                circle(drawing, current, 1, Scalar(0, 0, 255), -1);
             }
         }
         else {
             for (int j = initialPoint.y; j < img.rows; j++){
                 if (algoYStopOnCollision(img, lastColor, countChangeOfColorToIgnore, current, vectorLine, initialPoint, j))
                     break;
+                circle(drawing, current, 1, Scalar(0, 0, 255), -1);
             }
         }
     }
-    circle(drawing, current, 1, Scalar(0, 0, 255), -1);
-    return cv::Point2i();
+    //circle(drawing, current, 2, Scalar(0, 0, 255), -1);
+    hit.stoppingPoint = current;
+    int x = current.x - initialPoint.x;
+    int y = current.y - initialPoint.y;
+    hit.distanceSquared = (x * x) + (y * y);
+    return hit;
 }
 
-bool Raycast::hasColorChanged(Mat& img, uchar& lastColor, Point2i& toCheck) {
+
+bool Raycast::isPointInsideMat(const Mat &img, const Point2i &toCheck) {
+    Rect r = Rect(0, 0, img.cols, img.rows);
+    return toCheck.inside(r);
+}
+
+
+bool Raycast::hasColorChanged(const Mat& img, uchar& lastColor, const Point2i& toCheck) {
+    if (!isPointInsideMat(img, toCheck))
+        return true;
     uchar grayscale = img.at<uchar>(toCheck);
     if (grayscale != lastColor){
         lastColor = grayscale;
@@ -57,22 +77,21 @@ bool Raycast::hasColorChanged(Mat& img, uchar& lastColor, Point2i& toCheck) {
     return false;
 }
 
-void Raycast::progressPointOnLineFromXFormula(Point2i &pointToMove, Point2d& lineVec, Point2i& initialPoint, int &x) {
+void Raycast::progressPointOnLineFromXFormula(Point2i &pointToMove, const Point2d& lineVec, const Point2i& initialPoint, const int &x) {
     pointToMove.x = x;
     pointToMove.y = (int) (initialPoint.y + lineVec.y * (pointToMove.x - initialPoint.x) / lineVec.x);
 }
 
-void Raycast::progressPointOnLineFromYFormula(Point2i &pointToMove, Point2d& lineVec, Point2i& initialPoint, int &y) {
+void Raycast::progressPointOnLineFromYFormula(Point2i &pointToMove, const Point2d& lineVec, const Point2i& initialPoint, const int &y) {
     pointToMove.y = y;
     pointToMove.x = (int) (initialPoint.x + lineVec.x * (pointToMove.y - initialPoint.y) / lineVec.y);
 }
 
-bool Raycast::algoXStopOnCollision(Mat& img, uchar& lastColor, int& nbChangeToIgnore, 
-                                        Point2i& pointToMove, Point2d& lineVec,
-                                        Point2i& initialPoint, int& x) {
+bool Raycast::algoXStopOnCollision(const Mat& img, uchar& lastColor, int& nbChangeToIgnore, 
+                                        Point2i& pointToMove, const Point2d& lineVec,
+                                        const Point2i& initialPoint, const int& x) {
     progressPointOnLineFromXFormula(pointToMove, lineVec, initialPoint, x);
     if (hasColorChanged(img, lastColor, pointToMove)){
-        cout << "nbChangeToIgnore: " << nbChangeToIgnore << endl;
         if (nbChangeToIgnore != 0){
             nbChangeToIgnore--;
             return false;
@@ -82,12 +101,11 @@ bool Raycast::algoXStopOnCollision(Mat& img, uchar& lastColor, int& nbChangeToIg
     return false;
 }
 
-bool Raycast::algoYStopOnCollision(Mat& img, uchar& lastColor, int& nbChangeToIgnore, 
-                                        Point2i& pointToMove, Point2d& lineVec,
-                                        Point2i& initialPoint, int& y) {
+bool Raycast::algoYStopOnCollision(const Mat& img, uchar& lastColor, int& nbChangeToIgnore, 
+                                        Point2i& pointToMove, const Point2d& lineVec,
+                                        const Point2i& initialPoint, const int& y) {
     progressPointOnLineFromYFormula(pointToMove, lineVec, initialPoint, y);
     if (hasColorChanged(img, lastColor, pointToMove)){
-        cout << "nbChangeToIgnore: " << nbChangeToIgnore << endl;
         if (nbChangeToIgnore != 0){
             nbChangeToIgnore--;
             return false;
