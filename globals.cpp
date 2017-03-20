d3fc4a2b7a0794dd61b60ed6ea7533fb9306cd08
Antonin Lebrard <a.lebrard@gmail.com>
2017-03-20 12:37:51 +0100

    Update globals.cpp

--- globals.cpp
+++ globals.cpp
@@ -8,3 +8,33 @@
 bool Globals::stopAtThisFrame = false;
 int Globals::thresh = 100;
 int Globals::countIgnore = 60;
+
+void Globals::textOnImage(Mat &img, const double &d, const Point2i &p) {
+    int flooredVal = (int) d;
+    string s = to_string(flooredVal);
+    int xText = p.x, yText = p.y;
+    if (xText >= img.cols - 30)
+        xText = img.cols - 30;
+    if (xText < 0)
+        xText = 0;
+    if (yText >= img.rows - 10)
+        yText = img.rows - 10;
+    if (yText < 10)
+        yText = 10;
+    Point2i org = Point2i(xText, yText);
+    putText(img, s, org, FONT_HERSHEY_PLAIN, 1.0, Scalar(0,255,0));
+}
+
+void Globals::textOnImage(Mat &img, const string &s, const Point2i &p) {
+    int xText = p.x, yText = p.y;
+    if (xText >= img.cols - 30)
+        xText = img.cols - 30;
+    if (xText < 0)
+        xText = 0;
+    if (yText >= img.rows - 10)
+        yText = img.rows - 10;
+    if (yText < 10)
+        yText = 10;
+    Point2i org = Point2i(xText, yText);
+    putText(img, s, org, FONT_HERSHEY_PLAIN, 1.0, Scalar(0,255,0));
+}
