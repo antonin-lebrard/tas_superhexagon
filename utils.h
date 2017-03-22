29ae89aa515b8c2135a1c6b31511a5a8c673bcd2
Antonin Lebrard <a.lebrard@gmail.com>
2017-03-22 14:58:55 +0100

    Update utils.h

--- utils.h
+++ utils.h
@@ -20,6 +20,38 @@ struct WorkPoint {
     double angleBefore;
 };
 
+struct Zone {
+    vector<RaycastHit> hits;
+    int moyenneDistance = 0;
+    
+    int computeMoyenne() {
+        if (hits.size() == 0)
+            return 0;
+        int total = 0;
+        for (int i = 0; i < hits.size(); i++){
+            total += hits[i].distanceSquared;
+        }
+        moyenneDistance = (int) (total / hits.size());
+        return moyenneDistance;
+    }
+    
+    void push_back(RaycastHit hit){
+        hits.push_back(hit);
+    }
+    unsigned long size() const {
+        return hits.size();
+    }
+    void insert(vector<RaycastHit>::iterator it, RaycastHit hit ){
+        hits.insert(it, hit);
+    }
+    vector<RaycastHit>::iterator begin(){
+        return hits.begin();
+    }
+    RaycastHit operator[](int i) {
+        return hits[i];
+    }
+};
+
 
 class Utils {
 public:
@@ -34,9 +66,12 @@ private:
     static bool isOnBorder(const Point2i& p, const Mat& img);
     static Point2i direction(const Point2i& p1, const Point2i& p2);
     static double angle(const Point2i& p1, const Point2i& p2);
-    static WorkPoint constructWork(Mat& img, const RaycastHit& hit1, const RaycastHit& hit2, const Point2i& middle);
-    static vector<vector<RaycastHit>> createZones(Mat& img, const vector<RaycastHit>& collisions, const vector<WorkPoint>& workP);
-    static void drawZones(Mat& img, const vector<vector<RaycastHit>>& zones, const Point2i& middle);
+    static WorkPoint constructWork(const Mat& img, const RaycastHit& hit1, const RaycastHit& hit2, const Point2i& middle);
+    static vector<Zone> createZones(const Mat& img, const vector<RaycastHit>& collisions, const vector<WorkPoint>& workP);
+    static void drawZones(Mat& img, const vector<Zone>& zones, const Point2i& middle, const int& idxBest);
+    static int findBestZone(const Mat& img, const vector<Zone>& zones);
+
+    static vector<int> createOrdersForZones(vector<Zone> &zones, TriangleDetected &triangle);
 };
 
 
