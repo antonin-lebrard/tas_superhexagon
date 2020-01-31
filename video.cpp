62c60f19d1df8460567ccda9b0be5cdc314fbb11
Antonin Lebrard <antonin-lebrard@users.noreply.github.com>
2020-01-31 17:56:05 +0000

    Update video.cpp

--- video.cpp
+++ video.cpp
@@ -3,12 +3,15 @@
 //
 
 #include "video.h"
+#include "globals.h"
 
-void Video::openvideo(String videoName, Mat &frame) {
-    VideoCapture cap;
-    cap.open(videoName);
-    if(!cap.isOpened())  // check if we succeeded
+void Video::openVideo(const String& videoName, Mat &frame) {
+    VideoCapture cap = VideoCapture(videoName, CAP_FFMPEG);
+    // check if we succeeded
+    if(!cap.isOpened()) {
+        cout << "Capture did not succeed to open " << videoName << std::endl;
         return;
+    }
     cap >> frame;
     this->initCallback();
     while(cap.isOpened()){
@@ -17,11 +20,13 @@ void Video::openvideo(String videoName, Mat &frame) {
         this->drawCallback();
         bool signalFromProg = this->stopAtFrame();
         double timeTaken = (double)(clock() - tStart)/CLOCKS_PER_SEC;
-        //printf("Time taken: %.2fs\n", timeTaken*100.0);
+        printf("Time taken: %.2f ms\n", timeTaken*100.0);
         cap >> frame;
         char key = (char) waitKey(frameTime - (int)(timeTaken * 100.0 + 1));
-        if (signalFromProg)
+        if (signalFromProg || advanceOnlyOneFrame) {
             key = 'p';
+            advanceOnlyOneFrame = false;
+        }
         switch (key){
             case 'p':
                 paused = true;
@@ -33,12 +38,20 @@ void Video::openvideo(String videoName, Mat &frame) {
                         stopping = true;
                         paused = false;
                     }
+                    if (k == 'n') {
+                        paused = false;
+                        advanceOnlyOneFrame = true;
+                    }
+                    if (k == 'z')
+                        Globals::drawZones = !Globals::drawZones;
                 }
                 if (stopping)
                     return;
                 break;
             case 'k':
                 return;
+            case 'z':
+                Globals::drawZones = !Globals::drawZones;
             default:
                 break;
         }
